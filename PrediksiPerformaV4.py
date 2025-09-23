8aa8da92036d8de8e1c75510f3eaaa70b94aee01
AnandaNekyP <mr.anandanekyputra@gmail.com>
2025-09-23 17:15:22 +0700

    Adjust home menu layout for improved column balance and update heading tag for consistency

--- PrediksiPerformaV4.py
+++ PrediksiPerformaV4.py
@@ -304,13 +304,13 @@ main_container = st.empty()
 # ---------------------------
 if st.session_state["menu"] == "home":
     try:
-        nav_left, nav_center, nav_right = st.columns([4, 4, 4])
+        nav_left, nav_center, nav_right = st.columns([4, 5, 4])
         with nav_center:
             st.image(logo)
         st.write('<div class="title-container">', unsafe_allow_html=True)
         st.write("<h1>PrediksiPerforma</h1>", unsafe_allow_html=True)
         st.write(
-            "<h5>Prediksi Performa Akademik Mahasiswa dengan EDM Berbasis Supervised Learning Algorithm</h5>",
+            "<h6>Prediksi Performa Akademik Mahasiswa dengan EDM Berbasis Supervised Learning Algorithm</h6>",
             unsafe_allow_html=True,
         )
         st.write("</div>", unsafe_allow_html=True)
